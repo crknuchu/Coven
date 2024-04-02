0866d557dc580b73264a89cf1b3f456ec82e5d56
Mihailo Bogdanovic <mihailo.bogdanovic1@gmail.com>
2024-04-02 02:07:15 +0200

    add global inputs

---
+++ Global/Global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _input(event):
+	if Input.is_action_just_pressed("fullscreen"):
+		if get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
+			get_window().mode = Window.MODE_WINDOWED
+		else:
+			get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN
+	elif Input.is_action_just_pressed("kill_game"):
+		get_tree().quit()
